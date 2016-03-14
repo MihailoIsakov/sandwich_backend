92102419368aeb6597b889f1c74a076175c75adb
Mihailo Isakov <mihailo@protonmail.ch>
2016-03-14 02:17:29 +0100

    Added separate views for b92 and blic

--- views.py
+++ views.py
@@ -5,11 +5,11 @@ from flask.views import request
 from flask import jsonify
 #from flask.ext.jsonpify import jsonify
 
-from sandwich_backend import comments, reactions
+from sandwich_backend import blic, b92
 
 
-@app.route('/comment')
-def upsert_comment():
+@app.route('/blic')
+def blic_upsert_comment():
     userid = request.args.get('userid')                 # Goes in the user reaction
     comment_id = request.args.get('id')                 # Goes in the comment, used as ID
     link = request.args.get('link')                     # Goes in the comment, should be updated to the article page if found
@@ -29,7 +29,7 @@ def upsert_comment():
     else:
         ip = request.remote_addr
 
-    comments.update(
+    blic.comments.update(
         {
             'comment_id': comment_id, 
             'author': author,
@@ -46,10 +46,10 @@ def upsert_comment():
                 'downvotes': downvotes
             }
         },
-        upsert=True
+        upsert = True
     )
 
-    reactions.update(
+    blic.reactions.update(
         {
             'comment_id': comment_id,
             'userid': userid
@@ -66,11 +66,76 @@ def upsert_comment():
         upsert = True
     )
 
-    bot_count = reactions.find(
+    bot_count = blic.reactions.find(
         {'comment_id': comment_id, 'bot': 'true'}).count()
-    not_count = reactions.find(
+    not_count = blic.reactions.find(
         {'comment_id': comment_id, 'bot': 'false'}).count()
     json = {'bot_count': bot_count, 'not_count': not_count}
 
     result = jsonify(**json)
     return result
+
+
+@app.route('/b92')
+def b92_upsert_comment():
+    userid = request.args.get('userid')                 # Goes in the user reaction
+    comment_id = request.args.get('id')                 # Goes in the comment, used as ID
+    link = request.args.get('link')                     # Goes in the comment, should be updated to the article page if found
+    author = request.args.get('author')                 # Goes in the comment, used as ID
+    comment = request.args.get('comment')               # Goes in the comment
+    upvotes = request.args.get('upvotes')               # Goes in the comment
+    downvotes = request.args.get('downvotes')           # Goes in the comment
+    bot = request.args.get('bot')                       # Goes in the user reaction 
+
+    # meta
+    user_agent = request.headers['User-Agent']
+    language = request.headers['Accept-Language']
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+
+    b92.comments.update(
+        {
+            'comment_id': comment_id, 
+            'author': author,
+        },
+        {'$set':
+            {
+                'comment_id': comment_id, 
+                'link': link,
+                'comment': comment, 
+                'author': author,
+                'upvotes': upvotes,
+                'downvotes': downvotes
+            }
+        },
+        upsert=True
+    )
+
+    b92.reactions.update(
+        {
+            'comment_id': comment_id,
+            'userid': userid
+        },
+        {
+            '$set': {
+                'bot': bot,
+                'time': datetime.now(),
+                'user_agent': user_agent,
+                'language': language,
+                'ip': ip
+            }
+        },
+        upsert = True
+    )
+
+    bot_count = b92.reactions.find(
+        {'comment_id': comment_id, 'bot': 'true'}).count()
+    not_count = b92.reactions.find(
+        {'comment_id': comment_id, 'bot': 'false'}).count()
+    json = {'bot_count': bot_count, 'not_count': not_count}
+
+    result = jsonify(**json)
+    return result
+
